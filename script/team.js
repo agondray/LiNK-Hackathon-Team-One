46ec923d9cac4a7d6708c0aeded57b4da305e70d
unknown <honjae@gmail.com>
2015-08-30 14:37:23 -0700

    added members and activity photos from the factory

--- script/team.js
+++ script/team.js
@@ -1,22 +1,56 @@
 myApp.factory('teamFactory', function (){
 	factory = {}
-	messageboard = [{name:"Rod", title:"Hello?", date: new Date(), post:"New Content"}];
+	messageboard = [{name:"Jae Hong is FAbulous", title:"Hello. This is a content from the factory.", date: new Date(), post:"Objects of this message came from the factory, but you can hook up your backend and database easily to create a wonderful messageboard/bulletinboard!"}];
+	member = [{name: "Jason Jones" , image: "student1.png"},
+	{name: "Roy Hugh" , image: "student2.jpg"},
+	{name: "Jessica Palmer" , image: "student 4.jpg"},
+	{name: "Lory Park" , image: "student 5.jpg"}
+
+	]
+	activities = [
+	{activity: "Fundraiser", image: "assets/activity1.jpg"},
+   	{activity: "Group Photo Session", image: "assets/activity2.jpg"}
+	]
 	factory.teamChat = function(callback) {
 		callback(messageboard);
 	} 
 	factory.addChat = function(message){
 		messageboard.push(message);
 	}
+
+	factory.teamPics = function(callback){
+		callback(member);
+	}
+
+	factory.activityPics = function(callback){
+		callback(activities);
+	}
 	return factory;
+
 });
 
 myApp.controller('teamController', function ($scope, teamFactory){
 	messageboard();
+	
 	function messageboard() {
 		console.log("dog");
 		teamFactory.teamChat(function(data){
 			$scope.messages = data;
 			console.log($scope);
+		})
+	}
+	teamPics();
+	function teamPics(){
+		teamFactory.teamPics(function(data){
+			$scope.teampics = data;
+
+		})
+	}
+
+	activityPics();
+	function activityPics(){
+		teamFactory.activityPics(function(data){
+			$scope.activitypics = data;
 		})
 	}
 	console.log("Woof");
